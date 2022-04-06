5ea1f64a9c026f44687bfd75655997e1aa8e9377
SameepShah <samshah07@gmail.com>
2022-04-06 23:13:57 +0530

    Revert "First Comment for PlatformService"
    This reverts commit 5740fe74b101bd0d218407c6104d6dfee0f699a3.

--- PlatformService/DTOs/PlatformCreateDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace PlatformService.DTOs
-{
-    public class PlatformCreateDTO
-    {
-        public string Name { get; set; }
-        public string Publisher { get; set; }
-        public string Cost { get; set; }
-    }
-}
